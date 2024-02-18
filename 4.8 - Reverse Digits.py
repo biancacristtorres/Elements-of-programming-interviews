a8b31894442ad87f5e9359ced4b21d8d63e394e2
Bianca Torres <btorres@blueskyelearn.com>
2024-02-18 15:07:27 -0500

    4.8 exer

---
+++ 4.8 - Reverse Digits.py
@@ -0,0 +1,23 @@
+
+# name = input("What is your name? ")
+
+# print("Name: " + name)
+# print("Num: " + num)
+
+num = input("What is your favorite number? ")
+number = int(num)
+
+
+
+def revert(x):
+    result, x_remanining =0, abs(x)
+
+    while x_remanining:
+        result = result * 10 + x_remanining % 10
+        x_remanining //=10
+    return -result if x <0 else result    
+    
+
+result = revert(number)
+
+print(result)
